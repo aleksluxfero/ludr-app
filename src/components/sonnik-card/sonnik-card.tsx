abbb17026bdfd3f7b330f84d9861fa964a4b3bc0
luxfero <aleks.luxfero@gmail.com>
2023-02-16 12:35:03 +0500

    fix

--- src/components/sonnik-card/sonnik-card.tsx
+++ src/components/sonnik-card/sonnik-card.tsx
@@ -15,7 +15,6 @@ const SonnikCard: FC<SonnikCardProps> = ({popularSonnikName}) => {
   return (
     <div className={styles.sonnikCard}>
       <Link href={path}>
-        {/*<Image src={process.env.NEXT_PUBLIC_CLIENT_URL + "/img/lucid-dream.webp"} alt="sonnik" width={200} height={200}/>*/}
         <span className={styles.sonnikCardTitle}>{popularSonnikName}</span>
       </Link>
       <div className={styles.sonnikCardOverlay}/>
